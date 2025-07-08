3436c2b1933c6a8ef89d6f670a7bff8d616c2fee
Vladimir Volkov <volkovvova67@gmail.com>
2025-07-08 16:17:21 +0400

    chore: add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+/** @type {import('stylelint').Config} */
+export default {
+  extends: [
+    "stylelint-config-standard-scss",
+    "stylelint-config-idiomatic-order",
+  ],
+  plugins: ["stylelint-order"],
+  rules: {},
+}
